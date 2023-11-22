5da86acbf89a5c98bbf76bc21105b1c77abc6059
piyushjojo <impiyushjoshi@gmail.com>
2023-11-23 00:59:46 +0530

    Project Created using spring initilizer
    Signed-off-by: piyushjojo <impiyushjoshi@gmail.com>

---
+++ src/main/java/com/app/blogapp/BlogAppSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.app.blogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppSpringbootApplication.class, args);
+	}
+
+}
